ca8bb661f1ea9ec65dbc82f1e276f022cf7d67d5
Tiffani Patterson <71092406+tpatter1981@users.noreply.github.com>
2022-08-23 15:54:29 -0400

    Update main.js

--- MVC Tutorial/main.js
+++ MVC Tutorial/main.js
@@ -1,6 +1,6 @@
 var slides = document.querySelectorAll('#slides .slide');
 var currentSlide = 0;
-var slideInterval = setInterval(nextSlide,5000);
+var slideInterval = setInterval(nextSlide,12000);
 
 function nextSlide(){
     slides[currentSlide].className = 'slide';
